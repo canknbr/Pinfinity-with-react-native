3d5bd973c36ba4e45717b77a5198030fbb882e76
Can Kanbur <cankanbur1@gmail.com>
2023-02-23 18:14:37 +0300

    init

---
+++ src/screens/TabOneScreen.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../../types';
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View style={styles.container}>
+      <Text>Tab One</Text>
+      
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+ 
+});
