ea93bca85c5c3140b547458f078fb1ded9d23f28
Can Kanbur <cankanbur1@gmail.com>
2023-02-23 18:19:34 +0300

    added data

---
+++ assets/data/pins.ts
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: '0',
+    image:
+      'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'cat one',
+  },
+  {
+    id: '1',
+    image:
+      'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'cat two',
+  },
+  {
+    id: '2',
+    image:
+      'https://images.unsplash.com/photo-1618826411640-d6df44dd3f7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'cat three',
+  },
+  {
+    id: '3',
+    image:
+      'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    title: '',
+  },
+  {
+    id: '4',
+    image:
+      'https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'cat four',
+  },
+  {
+    id: '5',
+    image:
+      'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'cat five',
+  },
+  {
+    id: '6',
+    image:
+      'https://images.unsplash.com/photo-1606214174585-fe31582dc6ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'cat six',
+  },
+  {
+    id: '7',
+    image:
+      'https://images.unsplash.com/photo-1492370284958-c20b15c692d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'cat seven',
+  },
+  {
+    id: '8',
+    image:
+      'https://images.unsplash.com/photo-1491485880348-85d48a9e5312?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'cat eight',
+  },
+  {
+    id: '9',
+    image:
+      'https://images.unsplash.com/photo-1595752776689-aebef37b5d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjJ8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'cat nine',
+  },
+];
